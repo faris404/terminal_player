cd149669cba0c8a5933b8653fa00585f05837503
muhammed faris <faris.um2000@gmail.com>
2021-05-27 15:45:01 +0530

    interface change

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ let volum = 100
 //  setting play list
 let playList = []
 
-function getPlayList(){
+function getPlayList() {
     let dir = process.cwd()
     let files = fs.readdirSync(dir)
-    playList = files.filter(function(file) {
+    playList = files.filter(function (file) {
         return path.extname(file).toLowerCase() === '.wav' || path.extname(file).toLowerCase() === '.mp3';
     })
 
 }
 getPlayList()
-if (playList.length<1){
+if (playList.length < 1) {
     console.log('no musics in this directory');
     process.exit()
 }
@@ -29,12 +29,14 @@ const audio = new Player(playList[0])
 audio.setVolume(volum)
 
 
-async function nextSong(){
+async function nextSong() {
     playing++
-    if (playing < playListCount){
-        
+    if (playing < playListCount) {
+
         audio.next(playList[playing])
-    }else{
+        // console.log('current music - '+playList[playing]);
+        log(true)
+    } else {
         audio.destroy()
         process.exit()
 
@@ -42,29 +44,34 @@ async function nextSong(){
 }
 
 
-async function prevSong(){
-    
-    if (playing > 0){
+async function prevSong() {
+
+    if (playing > 0) {
         playing--
         audio.next(playList[playing])
-        audio.player.info().then(_=>{
+        audio.player.info().then(_ => {
             audio.play()
         })
+        // console.log('current music - '+playList[playing]);
+        log(true)
     }
+
 }
 
 
-function up(){
-    if (volum<200){
-        volum+=20
+function up() {
+    if (volum < 200) {
+        volum += 20
         audio.setVolume(volum)
+        log(true)
     }
 }
 
-function down(){
-    if (volum>20){
-        volum-=20
+function down() {
+    if (volum > 20) {
+        volum -= 20
         audio.setVolume(volum)
+        log(true)
     }
 }
 
@@ -76,40 +83,51 @@ audio.play()
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  })
+})
 
-console.log('Player Started');
-console.log(`stop | play | next | prev | up | down | exit `);
+function log(clear) {
+    if (clear)
+        process.stdout.write('\033c')
+    console.log("\x1b[1m","\x1b[31m",'-----------',"\x1b[38m",'Welcome to T-Player',"\x1b[31m",'-----------',"\x1b[34m")
+    console.log('current playing - ' + playList[playing])
+    console.log('current volume - ' + volum, "\x1b[33m")
+    console.log('--------------------------------------------------------');
+    console.log(`| cmds -: stop | play | next | prev | up | down | exit |`)
+    console.log('--------------------------------------------------------',"\x1b[36m"); 
+}
+log(true)
+// console.log('current music - '+playList[playing])
 rl.on('line', (input) => {
 
-    if (input=='stop')
+    if (input == 'stop')
         audio.pause()
-    
-    else if (input=='play')
+
+    else if (input == 'play')
         audio.play()
-    else if(input=='next')
+    else if (input == 'next')
         nextSong()
-    else if(input=='prev')
+    else if (input == 'prev')
         prevSong()
-    else if(input=='up')
+    else if (input == 'up')
         up()
-    else if(input=='down')
+    else if (input == 'down')
         down()
-    else if(input == 'exit'){
+    else if (input == 'exit') {
         audio.destroy()
         process.exit()
     }
+    log(true)
 
-    console.log(`stop | play | next | prev | up | down | exit `);
-  });
+});
 
 
 setInterval(() => {
-    audio.player.info().then(res=>{
+    audio.player.info().then(res => {
+
+        if (res.state === 'stopped' && isFirst) {
+            nextSong()
+        }
+        isFirst = true
         
-       if (res.state === 'stopped' && isFirst){
-           nextSong()
-       }
-       isFirst = true
-   })
+    })
 }, 1000);
